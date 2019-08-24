f61663c9b5e342deca2ac32c78ddd09e8e669cad
Joshua Hill <josh@jlhworkshop.com>
2019-08-24 17:34:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      
+      itemName = req.path.split()
       if Item.all.include?(itemName)
         resp.write "#{itemName.price}"
       else

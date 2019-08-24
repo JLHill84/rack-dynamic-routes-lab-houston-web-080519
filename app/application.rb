37255169ef6d0d5411d15b7e89bc4a8b69ad2f6f
Joshua Hill <josh@jlhworkshop.com>
2019-08-24 17:42:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,26 +1,26 @@
 class Application
   
   def call(env)
-    @resp = Rack::Response.new
-    @req = Rack::Request.new(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
     
-    if @req.path.match(/items/)
-      checkItems
+    if req.path.match(/items/)
+      resp.write "Hooray!"
     else
-      @resp.write "Route not found"
-      @resp.status = 404
+      resp.write "Route not found"
+      resp.status = 404
     end
     
   end
   
-  def checkItems
-    itemName = @req.path.split("/items/").last
-      if Item.all.include?(itemName)
-        @resp.write "#{itemName.price}"
-      else
-        @resp.write "Item not found"
-        @resp.status = 400
-      end
-  end
+  # def checkItems
+  #   itemName = @req.path.split("/items/").last
+  #     if Item.all.include?(itemName)
+  #       @resp.write "#{itemName.price}"
+  #     else
+  #       @resp.write "Item not found"
+  #       @resp.status = 400
+  #     end
+  # end
   
 end

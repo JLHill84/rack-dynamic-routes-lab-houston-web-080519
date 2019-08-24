b0a6993430fbc5ad851f8ab468b80bbcfbf7a44f
Joshua Hill <josh@jlhworkshop.com>
2019-08-24 17:45:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,14 +13,14 @@ class Application
     
   end
   
-  # def checkItems
-  #   itemName = @req.path.split("/items/").last
-  #     if Item.all.include?(itemName)
-  #       @resp.write "#{itemName.price}"
-  #     else
-  #       @resp.write "Item not found"
-  #       @resp.status = 400
-  #     end
-  # end
+  def checkItems
+    itemName = @req.path.split("/items/").last
+      if Item.all.include?(itemName)
+        @resp.write "#{itemName.price}"
+      else
+        @resp.write "Item not found"
+        @resp.status = 400
+      end
+  end
   
 end
